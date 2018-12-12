939e7f893beafb8d14dc93091d2fc9c8c4032b0c
SanderHestvik <sander_hestvik@hotmail.com>
2018-12-12 18:17:00 +0100

    Lagt til funksjon for å velge hvilke fag man skal ha med i utregningen

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Instruksjoner:
 2. Gå inn på fsweb.no og naviger til resultater
 3. Inspect element inne på siden (Ctrl+Shift+I i chrome eller høreklikk på siden og velg inspect)
 4. Velg console i den nye menyen som kommer opp, lim inn koden i tekstfeltet og trykk enter
+5. Du kan velge hvilke fag du vil ta med i utregningen ved i huke av checkboxene i stp. kolonnen
 
 Merk:
 - Dette skriptet vil ignorere fag som har ikke bestått eller bestått siden disse ikke er en del av snittet
--- fsweb-karaktersnitt-utregning.js
+++ fsweb-karaktersnitt-utregning.js
@@ -4,10 +4,15 @@ let dict = {"A":5, "B":4, "C":3, "D":2, "E":1}
 let total_grade_points = 0
 let total_student_points = 0
 let total_subject_with_grades = 0
+let average = 0
+let grades_dict = {}
+let resultater = document.getElementById("mineResultaterTittel") //Endrer et element som allerede er på siden
+
+
 for (let item of karakterer){
     if ((item.className === "none") || (item.className === "resultatTop")) //De klassene som inneholder karakterer for faget
     {
-        temp =[]
+        let temp =[]
         for (let x of item.childNodes){
             if (x.className === "col2Emne"){ //Legger til emnenavn
                 temp.push(x.childNodes[3].childNodes[1].innerText)
@@ -18,9 +23,16 @@ for (let item of karakterer){
                 }
                 points = dict[x.childNodes[x.childNodes.length-1].innerText] //Gjør bokstavkarakter om til poeng
                 temp.push(points); //Index 4 inneholder karakteren som string
+
             }
             else if (x.className === "col7Studiepoeng textAlignRight"){
                 temp.push(x.childNodes)//Hvis karakteren (Ingen studiepoeng) er forbedret vil childnodes være tom, fikser i neste loop
+                let input = document.createElement("input") //Legger til checkbox med hver karakter, slik at man manuelt kan ignorere karakterer
+                input.setAttribute("type", "checkbox")
+                input.setAttribute("checked", "true")
+                input.addEventListener("change", update)
+                x.appendChild(input)
+                temp.push(input)
             }
         }
         if((temp.length > 1)){ //Hvis listen bare har 1 element betyr det at faget ikke hadde karakter
@@ -28,30 +40,49 @@ for (let item of karakterer){
         }
     }
 }
+
+function update(){
+    total_subject_with_grades = 0
+    total_student_points = 0
+    total_grade_points = 0
+    Object.keys(grades_dict).forEach( x => {
+        if(grades_dict[x][2].checked) {
+            total_subject_with_grades++
+            total_grade_points += grades_dict[x][0] * grades_dict[x][1]
+            total_student_points += grades_dict[x][1]
+        }
+        }
+    )
+    average = (total_grade_points/total_student_points).toFixed(2)
+    console.log("Total points (grade*student points): " + total_grade_points)
+    console.log("Total student points: " + total_student_points)
+    console.log("Total subjects with grades: " + total_subject_with_grades)
+    console.log("Average: " + average)
+    resultater.innerText = "Du har fått karakter i " + total_subject_with_grades + " fag, og har et snitt på " + average
+}
+
+
 list = list.reverse() //Slik at elementet som inneholder studiepoeng kommer først
-let grades_dict = {}
 list.forEach(x => {
     if (x[0] in grades_dict){
-        grades_dict[x[0]][0] = Math.max(x[1], grades_dict[x[0]][0])
+            if(x[1]> grades_dict[x[0]][0]){
+                grades_dict[x[0]][0] = x[1]
+                let temp = grades_dict[x[0]][2]
+                grades_dict[x[0]][2] = x[3]
+                temp.parentNode.removeChild(temp) //Fjerner checkbox fra den karakteren som ikke er telt med
+            }
+            else{
+                x[3].parentNode.removeChild(x[3])
+            }
+
     }
     else {
-        grades_dict[x[0]] = [x[1], parseFloat(x[2][2].innerText.replace(",", "."))]
+        grades_dict[x[0]] = [x[1], parseFloat(x[2][2].innerText.replace(",", ".")), x[3]]
     }
 
 })
-Object.keys(grades_dict).forEach( x => {
-    total_subject_with_grades ++
-    total_grade_points += grades_dict[x][0]*grades_dict[x][1]
-    total_student_points += grades_dict[x][1]
-    }
-)
+
+update()
 console.log(grades_dict)
 
-let average = (total_grade_points/total_student_points).toFixed(2)
 
-console.log("Total points (grade*student points): " + total_grade_points)
-console.log("Total student points: " + total_student_points)
-console.log("Total subjects with grades: " + total_subject_with_grades)
-console.log("Average: " + average)
-let resultater = document.getElementById("mineResultaterTittel") //Endrer et element som allerede er på siden
-resultater.innerText = "Du har fått karakter i " + total_subject_with_grades + " fag, og har et snitt på " + average
