be9a1ca8729dd49cb1d6525721b720d7f8d7b92c
SanderHestvik <sander_hestvik@hotmail.com>
2019-02-09 00:45:03 +0100

    Turned it into a browser extention and change attribute names

--- fsweb-karaktersnitt-utregning.js
+++ fsweb-karaktersnitt-utregning.js
@@ -1,40 +1,40 @@
-let karakterer = document.getElementsByTagName("tr")
-let list = []
-let dict = {"A":5, "B":4, "C":3, "D":2, "E":1}
-let total_grade_points = 0
-let total_student_points = 0
-let total_subject_with_grades = 0
-let average = 0
-let grades_dict = {}
-let resultater = document.getElementById("mineResultaterTittel") //Endrer et element som allerede er på siden
+let karaktererGrid = document.getElementsByTagName("tr");
+let list = [];
+let GradesDict = {"A":5, "B":4, "C":3, "D":2, "E":1};
+let total_grade_points = 0;
+let total_student_points = 0;
+let total_subject_with_grades = 0;
+let average = 0;
+let grades_dict = {};
+let resultater = document.getElementById("mineResultaterTittel"); //Endrer et element som allerede er på siden
 
 
-for (let item of karakterer){
-    if ((item.className === "none") || (item.className === "resultatTop")) //De klassene som inneholder karakterer for faget
+for (let row of karaktererGrid){
+    if ((row.className === "none") || (row.className === "resultatTop")) //De klassene som inneholder karaktererGrid for faget
     {
-        let temp =[]
-        for (let x of item.childNodes){
-            if (x.className === "col2Emne"){ //Legger til emnenavn
-                temp.push(x.childNodes[3].childNodes[1].innerText)
+        let temp =[];
+        for (let col of row.childNodes){
+            if (col.className === "col2Emne"){ //Legger til emnenavn
+                temp.push(col.childNodes[3].childNodes[1].innerText)
             }
-            if (x.className === "col6Resultat textAlignRight"){ //Finner resultatkolonnen
-                if(!(x.childNodes[x.childNodes.length-1].innerText in dict)){ //Sjekker om faget har karakter, stopper løkken hvis ikke
+            if (col.className === "col6Resultat textAlignRight"){ //Finner resultatkolonnen
+                if(!(col.childNodes[col.childNodes.length-1].innerText in GradesDict)){ //Sjekker om faget har karakter, stopper løkken hvis ikke
                     break
                 }
-                points = dict[x.childNodes[x.childNodes.length-1].innerText] //Gjør bokstavkarakter om til poeng
+                points = GradesDict[col.childNodes[col.childNodes.length-1].innerText] //Gjør bokstavkarakter om til poeng
                 temp.push(points); //Index 4 inneholder karakteren som string
 
             }
-            else if (x.className === "col7Studiepoeng textAlignRight"){
-                if(x.childNodes.length < 3){
+            else if (col.className === "col7Studiepoeng textAlignRight"){
+                if(col.childNodes.length < 3){
                     break
                 }
-                temp.push(x.childNodes)//Hvis karakteren (Ingen studiepoeng) er forbedret vil childnodes være tom, fikser i neste loop
-                let input = document.createElement("input") //Legger til checkbox med hver karakter, slik at man manuelt kan ignorere karakterer
-                input.setAttribute("type", "checkbox")
-                input.setAttribute("checked", "true")
-                input.addEventListener("change", update)
-                x.appendChild(input)
+                temp.push(col.childNodes);//Hvis karakteren (Ingen studiepoeng) er forbedret vil childnodes være tom, fikser i neste loop
+                let input = document.createElement("input"); //Legger til checkbox med hver karakter, slik at man manuelt kan ignorere karaktererGrid
+                input.setAttribute("type", "checkbox");
+                input.setAttribute("checked", "true");
+                input.addEventListener("change", update);
+                col.appendChild(input);
                 temp.push(input)
             }
         }
@@ -45,22 +45,22 @@ for (let item of karakterer){
 }
 
 function update(){
-    total_subject_with_grades = 0
-    total_student_points = 0
-    total_grade_points = 0
+    total_subject_with_grades = 0;
+    total_student_points = 0;
+    total_grade_points = 0;
     Object.keys(grades_dict).forEach( x => {
         if(grades_dict[x][2].checked) {
-            total_subject_with_grades++
-            total_grade_points += grades_dict[x][0] * grades_dict[x][1]
+            total_subject_with_grades++;
+            total_grade_points += grades_dict[x][0] * grades_dict[x][1];
             total_student_points += grades_dict[x][1]
         }
         }
-    )
-    average = (total_grade_points/total_student_points).toFixed(2)
-    console.log("Total points (grade*student points): " + total_grade_points)
-    console.log("Total student points: " + total_student_points)
-    console.log("Total subjects with grades: " + total_subject_with_grades)
-    console.log("Average: " + average)
+    );
+    average = (total_grade_points/total_student_points).toFixed(2);
+    console.log("Total points (grade*student points): " + total_grade_points);
+    console.log("Total student points: " + total_student_points);
+    console.log("Total subjects with grades: " + total_subject_with_grades);
+    console.log("Average: " + average);
     resultater.innerText = ((total_subject_with_grades >1) ? ("I de " + total_subject_with_grades + " fagene") : ("I faget"))
         + " du har valgt, har du et snitt på " + average;
     if(total_subject_with_grades === 0) {resultater.innerText = "Du har ikke valgt noen fag"}
@@ -74,8 +74,8 @@ list.forEach(x => {
         grades_dict[x[0]] = [x[1], parseFloat(x[2][2].innerText.replace(",", ".")), x[3]]
     }
 
-})
+});
 
-update()
+update();
 
 
